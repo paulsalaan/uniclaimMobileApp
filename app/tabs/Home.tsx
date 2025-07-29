9b2e3932e4c42592185791aca506973b345aa486
paulsalaan <paulninosalaan17@gmail.com>
2025-07-29 17:30:07 +0800

    Changed dummy text to Second Dummy Text

--- app/tabs/Home.tsx
+++ app/tabs/Home.tsx
@@ -3,9 +3,7 @@ import { SafeAreaView, View } from "react-native";
 export default function Home() {
   return (
     <SafeAreaView>
-      <View>
-        dummy text
-      </View>
+      <View>Second Dummy Text</View>
     </SafeAreaView>
   );
 }
